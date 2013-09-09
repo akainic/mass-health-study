dc3f971862f1345c4558116c2c93974cdc0de994
akainic <annekainic@gmail.com>
2013-09-09 16:54:36 -0400

    Updated seed file, populated database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,29 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'CSV'
-require 
+
 
 filename = '/Users/annekainic/Dropbox/work/mass_health/mass_health_data_2/db/data/mass_chip_data.csv'
 
-CSV.foreach(filename, {:headers => true}) do |row|
-  town = data.where({
-    geography: row[0],
-    population_2005: row[1],
-    population_youth_2005: row[2],
-    population_senior_2005: row[3],
-    per_capita_income_2000: row[4],
-    infant_deaths_2005_2008: row[9],
-    teen_births_2005_2008: row[14]
-  }).first
+CSV.foreach(filename, {:headers => true}) do |attributes|
+
+  data = {
+    geography: attributes[0],
+    population_2005: attributes[1],
+    population_youth_2005: attributes[2],
+    population_senior_2005: attributes[3],
+    per_capita_income_2000: attributes[4],
+    infant_deaths_2005_2008: attributes[9],
+    teen_births_2005_2008: attributes[14]
+  }
+
+  town = Datum.where(data).first
 
   if town.nil?
-    town = health_data.new(row)
+    town = Datum.create(data)
   else
-    town.row = row
+    town.update_attributes(data)
   end
-
-  town.save!
-
 end
 
 
